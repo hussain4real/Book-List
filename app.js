36b672b748fa3293bd29c1e43ba1ce4ad34cc18b
Hussain Aminu <hussain_aminu@yahoo.com>
2019-11-05 17:58:22 +0300

    implement the Book class and UI class

--- app.js
+++ app.js
@@ -9,6 +9,41 @@ class Book {
 
 // UI Class: Handle UI Tasks
 
+class UI {
+  static displayBooks() {
+    const StoredBooks = [
+      {
+        title: 'Book One',
+        author: 'Hussain Aminu',
+        isbn: '5647555',
+      },
+      {
+        title: 'Book Two',
+        author: 'Suleiman Aishat',
+        isbn: '5675986',
+      },
+    ];
+
+    const books = StoredBooks;
+
+    books.forEach(book => UI.addBookToList(book));
+  }
+
+  static addBookToList(book) {
+    const list = document.querySelector('#book-list');
+
+    const row = document.createElement('tr');
+
+    row.innerHTML = `
+      <td>${book.title}</td>
+      <td>${book.author}</td>
+      <td>${book.isbn}</td>
+      <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
+      `;
+    list.appendChild(row);
+  }
+}
+
 // Store Class: Handles Storage
 
 // Event: Display Books
